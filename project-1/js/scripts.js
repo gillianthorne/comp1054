53a53d13d7928ee32d1b5072d24133e8d2a286b4
ducky <gillianthorne1353@gmail.com>
2026-04-13 10:42:31 -0400

    added a border-image to the header and tweaked the nav bar to be collapsed when looking at the header

--- project-1/js/scripts.js
+++ project-1/js/scripts.js
@@ -1,9 +1,20 @@
 nav = document.querySelector("nav");
+header = document.querySelector("header .container");
 
 nav.addEventListener("click", () => {
     if (nav.classList.contains("open")) {
         nav.classList.remove("open");
     } else {
         nav.classList.add("open");
+    }
+})
+
+// help with if statement from https://stackoverflow.com/questions/37513628/check-if-scrolled-past-div-with-javascript-no-jquery
+window.addEventListener("scroll", () => {
+    if (window.scrollY > header.offsetTop + header.offsetHeight) {
+        if (!nav.classList.contains("expanded")) nav.classList.add("expanded")
+    }
+    else {
+        if (nav.classList.contains("expanded")) nav.classList.remove("expanded")
     }
 })
